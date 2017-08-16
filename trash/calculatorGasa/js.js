2d1b4eb847f0bdd3875110625a8eeb831589a130
o78 <animenarutofanatuzumakinaruto@mail.ru>
2017-08-16 08:04:02 +0300

    -

--- trash/calculatorGasa/js.js
+++ trash/calculatorGasa/js.js
@@ -1,19 +1,19 @@
  function changeValues() {  
+			  let n0 = parseFloat(document.getElementById('n0').value);
 			  let n1 = parseFloat(document.getElementById('n1').value);
 			  let n2 = parseFloat(document.getElementById('n2').value);
 			  let n3 = parseFloat(document.getElementById('n3').value);
 			  let n4 = parseFloat(document.getElementById('n4').value);
-			  let n5 = parseFloat(document.getElementById('n5').value);
-			  let r1 = Math.round((n2-n3)*((n5/100)*n4));
-			  let r2 = Math.round(r1*365) ;
-			  let r3 =  Math.round(n1/r1);
-			  if (r1 > 0) {
+			  let r0 = Math.round((n1-n2)*((n4/100)*n3));
+			  let r1 = Math.round(r0*365) ;
+			  let r2 =  Math.round(n0/r0);
+			  if (r0 > 0) {
+			    document.getElementById("r0").innerHTML = r0;
 			    document.getElementById("r1").innerHTML = r1;
-			    document.getElementById("r2").innerHTML = r2;
-			    document.getElementById("r3").innerHTML = r3; }
+			    document.getElementById("r2").innerHTML = r2; }
 			  else {
-				    document.getElementById("r1").innerHTML = '0';
+				    document.getElementById("r0").innerHTML = '0';
+			    document.getElementById("r1").innerHTML = '0';
 			    document.getElementById("r2").innerHTML = '0';
-			    document.getElementById("r3").innerHTML = '0';
 				  }
 			  } 
